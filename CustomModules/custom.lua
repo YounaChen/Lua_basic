9a725342e8a03c7c75c141f36b893b13bc7d3cff
YounaChen <unitypro99@gmail.com>
2024-06-10 21:42:21 +0800

    Custom Modules

---
+++ CustomModules/custom.lua
@@ -0,0 +1,11 @@
+Mod = {
+    sum = function (x,y)
+        return x + y
+    end
+}
+
+function Mod.sayHello(name)
+    print("Hello ".. name)
+end
+
+return Mod
---
+++ CustomModules/main.lua
@@ -0,0 +1,5 @@
+local mod = require("custom")
+
+print(mod.sum(10, 5))
+
+mod.sayHello("Mike")
