d708f487fd4f1b70e7cf1a26a7276778379a3e2e
YounaChen <unitypro99@gmail.com>
2024-06-10 10:15:47 +0800

    print & comment

---
+++ Hello Lua/main.lua
@@ -0,0 +1,10 @@
+-- this is a single line comment
+print("Hello World")
+--[[
+    This is mutli-line comments
+    123456
+]]
+
+print("Hello World", "I am Lua")
+
+print("Hello World" .. " Let's learn Lua")
