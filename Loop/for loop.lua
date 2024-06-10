abab8f837edfd84433c97e101bbe49f5526989eb
YounaChen <unitypro99@gmail.com>
2024-06-10 14:20:15 +0800

    Loop

---
+++ Loop/for loop.lua
@@ -0,0 +1,33 @@
+-- for loop
+-- step 1
+for i = 1, 10 do
+    print(i)
+end
+
+print("=====================")
+-- step 2
+for i = 1, 10 ,2 do
+    print(i)
+end
+
+print("=====================")
+-- step -2
+for i =10, 1 ,-2 do
+    print(i)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Loop/while loop.lua
@@ -0,0 +1,30 @@
+local count = 0
+
+while true do
+    count = count + 1
+    print("Youna")
+
+    if count > 10 then
+        break
+    end
+end
+
+count = 0
+while count < 10 do
+    count = count + 1
+    print("Amy")
+end
+
+print("======================")
+
+local count = 10
+repeat
+    count = count + 1
+    print("mike")
+until count > 5
+
+
+
+
+
+
